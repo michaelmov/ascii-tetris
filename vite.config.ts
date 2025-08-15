5b47bd6612ffb58413f0e0c24b30b56c12a92bf9
Michael Movsesov <michael.movsesov@gmail.com>
2025-08-15 16:24:00 -0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,34 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
-export default defineConfig({
-  root: 'src',
-  base: './',
-  build: {
-    outDir: '../dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.css'],
-  },
-  css: {
-    devSourcemap: true,
-  },
+export default defineConfig(({ mode }) => {
+  const baseUrl = mode === 'production' 
+    ? 'https://michaelmov.github.io/ascii-tetris'
+    : 'http://localhost:5173';
+
+  return {
+    root: 'src',
+    base: './',
+    plugins: [
+      {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          return html
+            .replace(/%BASE_URL%/g, baseUrl + '/')
+            .replace(/%IMAGE_URL%/g, `${baseUrl}/images/ascii-tetris-min.png`);
+        },
+      },
+    ],
+    build: {
+      outDir: '../dist',
+      assetsDir: '',
+      emptyOutDir: true,
+      sourcemap: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.css'],
+    },
+    css: {
+      devSourcemap: true,
+    },
+  };
 });
